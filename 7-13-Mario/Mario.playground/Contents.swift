de8af117aee145e728ab4a3c059eac8a06d63c0b
Student <student@macbook-pro-peterson.local>
2016-12-05 08:45:50 -0500

    Commit code for Mario challenge

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -11,3 +11,22 @@
  */
 
 // Add your code below
+func levelCost(heights : [Int], maxJump : Int) -> Int {
+    var points = 0
+    for i in stride(from: 0, through: heights.count, by: 1) {
+        if (i < heights.count - 1) {
+            if (heights[i] == heights[i + 1]) {
+                points += 1
+            } else if (heights[i] < heights[i + 1] && abs(heights[i + 1] - heights[i]) <= maxJump) {
+                let addedPoints = 2 * (abs(heights[i + 1] - heights[i]))
+                points += addedPoints
+            } else if (heights[i] > heights[i + 1] && abs(heights[i] - heights[i + 1]) <= maxJump) {
+                let addedPoints = 2 * (abs(heights[i] - heights[i + 1]))
+                points += addedPoints
+            } else {
+                return -1
+            }
+        }
+    }
+    return points
+}
