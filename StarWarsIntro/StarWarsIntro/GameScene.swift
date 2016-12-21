c7c88afc671a9fc2cf185eb843677e5887ae81b0
Ethan Peterson <epeterson@rsgc.on.ca>
2016-12-21 08:45:12 -0500

    Added stars to the star wars intro animation and divided different segments of code into organized functions such as makeStars(), showTitle() and playMusic()

--- StarWarsIntro/StarWarsIntro/GameScene.swift
+++ StarWarsIntro/StarWarsIntro/GameScene.swift
@@ -11,15 +11,43 @@ import GameplayKit
 
 class Scene: SKScene {
     
+    var midPoint : CGPoint? // useful CGPoint object allowing me to just type midpoint instead of the x and y coordinates for the center of the screen.
+    
+    func showTitle() {
+        backgroundColor = SKColor.black
+        let title = SKSpriteNode(imageNamed: "Starwars-logo.png")
+        title.setScale(0.43)
+        title.position = midPoint!
+        scene?.addChild(title) // display star wars title text
+        let titleScaleDown = SKAction.scale(to: 0, duration: 3.5)
+        title.run(titleScaleDown) // run the SKAction animation
+    }
+    
+    func makeStars() {
+        let starEmitter = SKEmitterNode()
+        starEmitter.particleLifetime = 40
+        starEmitter.particleBlendMode = SKBlendMode.alpha
+        starEmitter.particleBirthRate = 3
+        starEmitter.particleSize = CGSize(width : 3, height : 3)
+        starEmitter.particleColor = SKColor(red : 100, green : 100, blue : 255, alpha : 1)
+        starEmitter.position = midPoint! // make star particles spawn from center of screen behind star wars logo
+        starEmitter.particleSpeed = 16
+        starEmitter.particleSpeedRange = 100
+        starEmitter.particlePositionRange = CGVector(dx : frame.size.width, dy : frame.size.height) // allow particles to occupy any position onscreen in their movements
+        starEmitter.emissionAngle = 3.14
+        starEmitter.advanceSimulationTime(40)
+        starEmitter.particleAlpha = 0.5
+        starEmitter.particleAlphaRange = 0.5
+        scene?.addChild(starEmitter) // display the particles by adding emitter object to scene
+    }
+    
+    func playMusic() {
+        
+    }
     
     override func didMove(to view: SKView) {
-        // Set the background color
-        backgroundColor = SKColor.black
-        let moveTextUp = SKAction.moveBy(x: 0, y: 30, duration: 10)
-        let title = SKSpriteNode(imageNamed: "Starwars-logo.png")
-        let midPoint = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0)
-        title.setScale(0.43)
-        title.position = midPoint
-        scene?.addChild(title)
+        midPoint = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0)
+        showTitle()
+        makeStars()
     }
 }
